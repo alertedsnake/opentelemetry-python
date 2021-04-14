cb7d60c321adb0d9a2818e90d9e5e09c0422fda4
github-action-benchmark <github@users.noreply.github.com>
2021-04-14 18:51:50 +0000

    add OpenTelemetry Python Benchmarks - Python pypy3 - core (pytest) benchmark result for b73d8009904fc1693b2ba3a1e0656af376f859cf

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618426277708,
+  "lastUpdate": 1618426310542,
   "repoUrl": "https://github.com/alertedsnake/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.5 - core": [
@@ -618,6 +618,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000007764878412286327",
             "extra": "mean: 5.499636696933928 usec\nrounds: 119048"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b73d8009904fc1693b2ba3a1e0656af376f859cf",
+          "message": "no longer pin sphinx (#1766)",
+          "timestamp": "2021-04-13T15:24:03-07:00",
+          "tree_id": "86e3eaf4c5a1ca4b4b266b61f79310ea5ef641cb",
+          "url": "https://github.com/alertedsnake/opentelemetry-python/commit/b73d8009904fc1693b2ba3a1e0656af376f859cf"
+        },
+        "date": 1618426309131,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 269662.56526487565,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001719434425652264",
+            "extra": "mean: 3.7083382301052836 usec\nrounds: 42014"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 172817.79489353302,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000005932364742349306",
+            "extra": "mean: 5.78644115101726 usec\nrounds: 135117"
           }
         ]
       }
